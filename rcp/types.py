ac40122aab1d9b609aa9642ec3d7f77d98297565
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-07-20 21:37:17 +0530

    feat : HTTP request and response types for events - renamed Type -> ScopeType

--- rcp/types.py
+++ rcp/types.py
@@ -1,5 +1,15 @@
 from enum import StrEnum
 
-class Type(StrEnum):
+class ScopeType(StrEnum):
     HTTP = "HTTP"
     WEBTRANSPORT = "WEBTRANSPORT" # Reserved for future RCP versions
+
+class HTTPRequestType(StrEnum):
+    REQUEST = "HTTP.REQUEST"
+
+class HTTPResponseType(StrEnum):
+    START = "HTTP.RESPONSE.START"
+    BODY = "HTTP.RESPONSE.BODY"
+    TRAILERS = "HTTP.RESPONSE.TRAILERS"
+    DISCONNECT = "HTTP.RESPONSE.DISCONNECT"
+    DEBUG = "HTTP.RESPONSE.DEBUG"
