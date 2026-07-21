915a4e8b0ba89759ab7e64af92d3b9a1b1e4716a
Bhavya Malhotra <242802181+code-1py@users.noreply.github.com>
2026-07-21 06:46:36 +0530

    feat(scope): add HTTP scope definition with request metadata and connection info

---
+++ rcp/rcp.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+from typing import TypedDict
+
+class RCPVersions(StrEnum):
+    VERSION_1 = "1.0"
+
+class RCP(TypedDict):
+    version = RCPVersions
--- rcp/scopes.py
+++ rcp/scopes.py
@@ -1,6 +1,26 @@
 from __future__ import annotations
 
-from .types import (HTTPRequestEventType,HTTPResponseEventType,ScopeType)
+from .types import HTTPRequestEventType, HTTPResponseEventType, ScopeType, HTTPVersions
 from .methods import RequestMethod
 from typing import NotRequired ,Any, Literal, TypedDict
+from collections.abc import Awaitable, Callable, Iterable, MutableMapping
+from .rcp import RCP
+from .scheme import HTTPScheme
 
+Headers = Iterable[tuple[bytes, bytes]]
+
+class HTTPScope(TypedDict):
+    type: ScopeType
+    rcp: RCP
+    http_version: HTTPVersions
+    method: RequestMethod
+    scheme: HTTPScheme
+    path: str
+    raw_path: bytes
+    query_string: bytes
+    root_path: str
+    headers: Headers
+    client: tuple[str, int] | None
+    server: tuple[str, int | None] | None
+    state: NotRequired[dict[str, Any]]
+    extensions: NotRequired[dict[str, dict[object, object]]]
